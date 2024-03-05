2aaef5b712200c5baf4d4102d29fd53c6ddb8b5c
Soar Qin <soarchin@gmail.com>
2024-03-06 02:59:56 +0800

    WIP

--- bingotool/cell.cpp
+++ bingotool/cell.cpp
@@ -5,6 +5,7 @@
 
 #include <SDL3_gfxPrimitives.h>
 
+extern SDL_Window *gWindow;
 Cells gCells;
 
 void Cell::updateTexture() {
@@ -17,30 +18,42 @@ void Cell::updateTexture() {
         return;
     }
     auto *font = gConfig.font;
-    auto fontSize = gConfig.fontSize;
-    while (true) {
-        auto wrapLength = gConfig.cellSize[0] * 9 / 10;
-        auto *surface =
-            TTF_RenderUTF8_BlackOutline_Wrapped(font, text.c_str(), &gConfig.textColor, wrapLength, &gConfig.textShadowColor, gConfig.textShadow, gConfig.textShadowOffset);
-        if (surface->h <= gConfig.cellSize[1] * 9 / 10) {
-            texture = SDL_CreateTextureFromSurface(renderer, surface);
-            SDL_DestroySurface(surface);
-            break;
+    if (false) {
+        auto fontSize = gConfig.fontSize;
+        auto maxHeight = gConfig.cellSize[1] * 9 / 10;
+        while (true) {
+            if (calculateMinimalHeight(font) <= maxHeight) {
+                auto *surface =
+                    TTF_RenderUTF8_BlackOutline_Wrapped(font,
+                                                        text.c_str(),
+                                                        &gConfig.textColor,
+                                                        gConfig.cellSize[0] * 9 / 10,
+                                                        &gConfig.textShadowColor,
+                                                        gConfig.textShadow,
+                                                        gConfig.textShadowOffset);
+                texture = SDL_CreateTextureFromSurface(renderer, surface);
+                SDL_DestroySurface(surface);
+                break;
+            }
+            fontSize--;
+            font = TTF_OpenFont(gConfig.fontFile.c_str(), fontSize);
+            TTF_SetFontStyle(font, gConfig.fontStyle);
+            TTF_SetFontWrappedAlign(font, TTF_WRAPPED_ALIGN_CENTER);
         }
+        if (font != gConfig.font) {
+            TTF_CloseFont(font);
+        }
+    } else {
+        auto *surface =
+            TTF_RenderUTF8_BlackOutline_Wrapped(font, text.c_str(), &gConfig.textColor, gConfig.cellSize[0] * 9 / 10, &gConfig.textShadowColor, gConfig.textShadow, gConfig.textShadowOffset);
+        texture = SDL_CreateTextureFromSurface(renderer, surface);
         SDL_DestroySurface(surface);
-        fontSize--;
-        font = TTF_OpenFont(gConfig.fontFile.c_str(), fontSize);
-        TTF_SetFontStyle(font, gConfig.fontStyle);
-        TTF_SetFontWrappedAlign(font, TTF_WRAPPED_ALIGN_CENTER);
-    }
-    if (font != gConfig.font) {
-        TTF_CloseFont(font);
     }
     SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
     SDL_QueryTexture(texture, nullptr, nullptr, &w, &h);
 }
 
-void Cell::render(int x, int y, int cx, int cy) {
+void Cell::render(int x, int y, int cx, int cy) const {
     bool dbl = status > 2;
     auto fx = (float)x, fy = (float)y, fcx = (float)cx, fcy = (float)cy;
     auto st = dbl ? (status - 2) : status;
@@ -73,12 +86,89 @@ void Cell::render(int x, int y, int cx, int cy) {
         SDL_RenderTexture(renderer, texture, nullptr, &rect);
     }
 }
+
+int Cell::calculateMinimalWidth(TTF_Font *font) {
+    auto maxHeight = gConfig.cellSize[1] * 9 / 10;
+    auto width = gConfig.cellSize[0] & ~1;
+    size_t len = text.length();
+    int tw, th;
+    TTF_SizeText(font, text.c_str(), &tw, &th);
+    th = std::max(th, TTF_FontHeight(font));
+    while (true) {
+        auto maxWidth = width * 9 / 10;
+        size_t index = 0;
+        int lines = 0;
+        while (index < len) {
+            int count;
+            if (TTF_MeasureUTF8(font, text.c_str() + index, maxWidth, nullptr, &count) < 0) break;
+            if (count == 0) break;
+            lines++;
+            while (count > 0 && index < len) {
+                auto c = uint8_t(text[index]);
+                if (c < 0x80) index++;
+                else if (c < 0xE0) index += 2;
+                else if (c < 0xF0) index += 3;
+                else if (c < 0xF8) index += 4;
+                else if (c < 0xFC) index += 5;
+                else index += 6;
+                count--;
+            }
+        }
+        auto currHeight = lines * th;
+        if (currHeight <= maxHeight) break;
+        width += 2;
+    }
+    return width;
+}
+
+int Cell::calculateMinimalHeight(TTF_Font *font) {
+    auto maxWidth = gConfig.cellSize[0] * 9 / 10;
+    size_t index = 0;
+    size_t len = text.length();
+    int lines = 0;
+    while (index < len) {
+        int count;
+        if (TTF_MeasureUTF8(font, text.c_str() + index, maxWidth, nullptr, &count) < 0) break;
+        if (count == 0) break;
+        lines++;
+        while (count > 0 && index < len) {
+            auto c = uint8_t(text[index]);
+            if (c < 0x80) index++;
+            else if (c < 0xE0) index += 2;
+            else if (c < 0xF0) index += 3;
+            else if (c < 0xF8) index += 4;
+            else if (c < 0xFC) index += 5;
+            else index += 6;
+            count--;
+        }
+    }
+    int tw, th;
+    TTF_SizeText(font, text.c_str(), &tw, &th);
+    return lines * std::max(th, TTF_FontHeight(font));
+}
+
 void Cells::init(SDL_Renderer *renderer) {
+    fitCellsForText();
     for (auto &r : cells_) {
         for (auto &c : r) {
             c.renderer = renderer;
             c.updateTexture();
         }
+    }
+}
+
+void Cells::fitCellsForText() {
+    int minWidth = gConfig.cellSize[0];
+    foreach([&minWidth](Cell &cell, int, int) {
+        int w = cell.calculateMinimalWidth(gConfig.font);
+        if (w > minWidth) minWidth = w;
+    });
+    if (minWidth > gConfig.cellSize[0]) {
+        gConfig.cellSize[0] = minWidth;
+        SDL_SetWindowSize(gWindow,
+                          gConfig.cellSize[0] * 5 + gConfig.cellSpacing * 4 + gConfig.cellBorder * 2,
+                          gConfig.cellSize[1] * 5 + gConfig.cellSpacing * 4 + gConfig.cellBorder * 2);
+        SDL_SetWindowPosition(gWindow, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
     }
 }
 
--- bingotool/cell.h
+++ bingotool/cell.h
@@ -1,12 +1,15 @@
 #pragma once
 
 #include <SDL3/SDL.h>
+#include <SDL3_ttf/SDL_ttf.h>
 #include <string>
 #include <functional>
 
 struct Cell {
     void updateTexture();
-    void render(int x, int y, int cx, int cy);
+    void render(int x, int y, int cx, int cy) const;
+    int calculateMinimalWidth(TTF_Font *font);
+    int calculateMinimalHeight(TTF_Font *font);
 
     SDL_Renderer *renderer = nullptr;
     SDL_Texture *texture = nullptr;
@@ -19,6 +22,7 @@ class Cells {
 public:
     void init(SDL_Renderer *renderer);
     Cell *operator[](int index) { return cells_[index]; }
+    void fitCellsForText();
     void foreach(const std::function<void(Cell &cell, int x, int y)> &callback);
 
 private:
