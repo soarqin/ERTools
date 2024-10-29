fbcd098fec307dca6f763a3783ca73ff70f2282e
Soar Qin <soarchin@gmail.com>
2024-10-30 02:48:04 +0800

    WIP

--- modloader/steam/vdf.h
+++ modloader/steam/vdf.h
@@ -16,10 +16,10 @@ typedef struct vdf_node_t vdf_node_t;
 extern vdf_node_t *vdf_open(const wchar_t *path);
 extern void vdf_free(vdf_node_t *vdf);
 extern vdf_node_t *vdf_get_child(vdf_node_t *vdf, const char *key);
-extern void void vdf_iterate(vdf_node_t *vdf,
-                             void (*child_callback)(const char *key, vdf_node_t **node, int count, void *data),
-                             void (*value_callback)(const char *key, const char *value, void *data),
-                             void *data);
+extern void vdf_iterate(vdf_node_t *vdf,
+                        void (*child_callback)(const char *key, vdf_node_t **node, int count, void *data),
+                        void (*value_callback)(const char *key, const char *value, void *data),
+                        void *data);
 extern bool vdf_is_leaf(vdf_node_t *vdf);
 extern const char *vdf_get_key(vdf_node_t *vdf);
 extern const char *vdf_get_value(vdf_node_t *vdf);
